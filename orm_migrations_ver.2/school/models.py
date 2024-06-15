39fd2992ddae296ed02c9266fb641a2595815399
StanislavBalov <123889387+StanislavBalov@users.noreply.github.com>
2024-06-15 22:16:09 +0300

    orm_migrations_ver.2

---
+++ orm_migrations_ver.2/school/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    subject = models.CharField(max_length=100, verbose_name='Предмет')
+
+    class Meta:
+        verbose_name = 'Учитель'
+        verbose_name_plural = 'Учителя'
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    teacher = models.ManyToManyField(Teacher, related_name='students', on_delete=models.CASCADE)
+    group = models.CharField(max_length=100, verbose_name='Класс')
+
+    class Meta:
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
+
+    def __str__(self):
+        return self.name
